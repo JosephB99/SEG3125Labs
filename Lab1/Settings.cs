5a54b54b09fbf3879a64eea6d2ab65269d7bbcb3
Josh <jarhe058@uottawa.ca>
2020-02-03 10:18:35 -0500

    Improved Settings

--- Lab1/Settings.cs
+++ Lab1/Settings.cs
@@ -14,7 +14,6 @@ namespace Lab1
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = false)]
     public class Settings : AppCompatActivity
     {
-        private Button SaveButton;
         public static int NoOfQuestions;
         public static int PassingGrade = 50;
 
@@ -24,32 +23,54 @@ namespace Lab1
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             SetContentView(Resource.Layout.settingsActivity);
 
-            EditText QuestionCntText = FindViewById<EditText>(Resource.Id.QuestionCountText);
-            EditText PassGrdText = FindViewById<EditText>(Resource.Id.PassGradeText);
-            string EditTxt1 = QuestionCntText.Text.ToString();
-            string EditTxt2 = PassGrdText.Text.ToString();
-            if (string.IsNullOrEmpty(EditTxt1) & string.IsNullOrEmpty(EditTxt2))
-            {
-                return;
-            }
-            else
-            {
-                SaveButton = FindViewById<Button>(Resource.Id.SaveBtn);
-                SaveButton.Text = "Save";
-                SaveButton.Click += SaveBtnClick;
-            }
-            
         }
 
         private void SaveBtnClick(object sender, EventArgs e)
         {
+
             EditText QuestionCntText = FindViewById<EditText>(Resource.Id.QuestionCountText);
-            NoOfQuestions = int.Parse(QuestionCntText.Text.ToString());
             EditText PassGrdText = FindViewById<EditText>(Resource.Id.PassGradeText);
+            int EditTxt1 = int.Parse(QuestionCntText.Text.ToString());
+            int EditTxt2 = int.Parse(PassGrdText.Text.ToString());
+            if (string.IsNullOrEmpty(QuestionCntText.Text) | string.IsNullOrEmpty(PassGrdText.Text))
+            {
+                DisplayAlert("Empty Field", "Please fill in the parameters");
+            }
+
+            if (EditTxt2 > 100 | EditTxt2 < 0)
+            {
+                DisplayAlert("Invalid Choice", "The percentage has to be within 0 and 100!");
+            }
+             
+            if (QuestionCntText.Text is string | PassGrdText.Text is string)
+            {
+                DisplayAlert("Invalid Choice", "The parameter only takes integers!");
+            }
+           
+            NoOfQuestions = int.Parse(QuestionCntText.Text.ToString());
             PassingGrade = int.Parse(PassGrdText.Text.ToString());
 
         }
 
+        private void DisplayAlert(string title, string message, bool returnHome = false)
+        {
+            Android.App.AlertDialog.Builder dialog = new Android.App.AlertDialog.Builder(this);
+            Android.App.AlertDialog alert = dialog.Create();
+            alert.SetTitle(title);
+            alert.SetMessage(message);
+            alert.SetButton("OK", (c, ev) =>
+            {
+                alert.Hide();
 
+                if (returnHome)
+                {
+                    Intent mainPage = new Intent(this, typeof(MainActivity));
+                    StartActivity(mainPage);
+                }
+            });
+            alert.Show();
+
+
+        }
     }
 }
